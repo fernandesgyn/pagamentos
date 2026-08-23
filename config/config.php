da830ec7a670d55a643f337a683af1e070f64818
fernandesgyn <58429618+fernandesgyn@users.noreply.github.com>
2026-08-23 17:18:41 -0300

    feat: adiciona constantes da aplicação

---
+++ config/config.php
@@ -0,0 +1,5 @@
+<?php
+declare(strict_types=1);
+define('APP_NAME', getenv('APP_NAME') ?: 'Sistema de Liquidação e Pagamentos');
+define('APP_URL', getenv('APP_URL') ?: 'http://localhost:8000');
+define('BASE_PATH', dirname(__DIR__));
